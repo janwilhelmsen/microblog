4e3c4b995e754096ffed4a117283b09f4b1b47cd
Jan Wilhelmsen <janwil@gmail.com>
2024-01-29 12:21:52 +0100

    Chapter 7 ,done.error and logging

--- config.py
+++ config.py
@@ -4,4 +4,10 @@ class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY_FLASK') or 'you_will_never_guess'
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir,'app.db')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['janwil@gmail.com']
 
